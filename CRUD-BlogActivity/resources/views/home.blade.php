4fd202f50d3c7f65c4f170f56da60e18594ec7e0
Wilson-John Tadlip <infinite_offence@yahoo.com>
2023-09-09 03:16:05 +0800

    simple register and logout

---
+++ CRUD-BlogActivity/resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Test Blog</title>
+</head>
+
+<body>
+
+    @auth
+        <p>Congratulations, you are logged in!</p>
+        <form action="/logout" method="POST">
+            @csrf
+            <button>Logout</button>
+        </form>
+    @else
+        <div style="border: 3px solid black;">
+            <h2>Register</h2>
+            <form action="/register" method="POST">
+                @csrf
+                <input name="name" type="text" placeholder="name">
+                <input name="email" type="text" placeholder="email">
+                <input name="password" type="password" placeholder="password">
+                <button>Register</button>
+            </form>
+        </div>
+    @endauth
+</body>
+
+</html>
+
+
+
+
